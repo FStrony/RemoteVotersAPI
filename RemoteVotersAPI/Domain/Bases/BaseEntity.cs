36e87985c4899875e64b63aaee2f1660ab182311
FStrony <fstrony@gmail.com>
2020-10-24 17:48:31 -0300

    Add Entities

---
+++ RemoteVotersAPI/Domain/Bases/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MongoDB.Bson;
+using Newtonsoft.Json;
+using RemoteVotersAPI.Utils;
+
+namespace RemoteVotersAPI.Domain.Bases
+{
+    public class BaseEntity
+    {
+        [JsonConverter(typeof(ObjectIdConverter))]
+        public ObjectId Id { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public DateTime? ModificationDate { get; set; }
+
+        public BaseEntity()
+        {
+            Id = ObjectId.GenerateNewId();
+            RegistrationDate = DateTime.Now;
+        }
+    }
+}
---
+++ RemoteVotersAPI/Domain/Bases/BaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using RemoteVotersAPI.Infra.ModelSettings;
+
+namespace RemoteVotersAPI.Domain.Bases
+{
+    public class BaseRepository
+    {
+        private readonly MongoDBConfig _mongoDBConfig = null;
+        protected MongoClient client { get; private set; }
+
+        public BaseRepository(IOptions<MongoDBConfig> mongoDBConfig)
+        {
+            _mongoDBConfig = mongoDBConfig.Value;
+            client = new MongoClient(_mongoDBConfig.getConnectionString());
+            DataBase = client.GetDatabase(_mongoDBConfig.Database);
+        }
+
+        public IMongoDatabase DataBase { get; private set; }
+    }
+}
