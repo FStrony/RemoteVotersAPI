f8d466810b4954f28c1fc0cdd2da8e7fcdbbad56
FStrony <fstrony@gmail.com>
2020-11-01 13:56:31 -0300

    Add Vote Auth Mock

---
+++ RemoteVotersAPI/Controllers/VoteAuthMock.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace remotevotersapi.Controllers
+{
+
+    /// <summary>
+    /// Vote Auth Mock Controller
+    ///
+    /// Author: FStrony
+    /// </summary> 
+    [ApiController]
+    [Route("[controller]")]
+    [Produces("application/json")]
+    public class VoteAuthMock : ControllerBase
+    {
+        /// <summary>
+        /// POST Success Auth Mock
+        /// </summary>
+        /// <param name="properties"></param>
+        /// <returns></returns>
+        [HttpPost("success")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public bool AuthSuccess([FromBody] Dictionary<String, String> properties)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// POST Fail Auth Mock
+        /// </summary>
+        /// <param name="properties"></param>
+        /// <returns></returns>
+        [HttpPost("fail")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public bool AuthFail([FromBody] IDictionary<String, String> properties)
+        {
+            return false;
+        }
+    }
+}
