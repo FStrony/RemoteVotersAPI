8d748e432d9598a3c8a48f06ebfc8c70770bb400
FStrony <fstrony@gmail.com>
2020-11-22 18:38:31 -0300

    Add Custom Exceptions

---
+++ RemoteVotersAPI/Utils/Exceptions/NonUniqueRecordException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace remotevotersapi.Application.Services
+{
+    /// <summary>
+    /// Non Unique Record Exception
+    ///
+    /// Author: FStrony
+    /// </summary>
+    [Serializable]
+    internal class NonUniqueRecordException : Exception
+    {
+        public NonUniqueRecordException()
+        {
+        }
+
+        public NonUniqueRecordException(string message) : base(message)
+        {
+        }
+
+        public NonUniqueRecordException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NonUniqueRecordException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ RemoteVotersAPI/Utils/Exceptions/NotFoundException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace remotevotersapi.Application.Services
+{
+    /// <summary>
+    /// Not Found Exception
+    ///
+    /// Author: FStrony
+    /// </summary>
+    [Serializable]
+    internal class NotFoundException : Exception
+    {
+        public NotFoundException()
+        {
+        }
+
+        public NotFoundException(string message) : base(message)
+        {
+        }
+
+        public NotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
